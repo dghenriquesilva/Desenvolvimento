c09a2000d1c06e54c5b6193ca53f89f484ff7bf0
Diogo h <dghenrique@gmail.com>
2021-01-23 15:38:21 -0300

    Inicio de projeto

---
+++ Empresa/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Empresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Como voce esta hoje");
+            Console.WriteLine("Tenha um bom dia");
+        }
+    }
+}
